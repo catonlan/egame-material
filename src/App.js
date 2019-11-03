0319805afb96a63a713792c55020ef8f8561ac55
catonlan <catonlan@163.com>
2019-11-03 20:09:18 +0800

    定义页面路由

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {connect, } from 'react-redux';
+import {Route, Switch, } from 'react-router-dom';
 import myFun from './assets/js/myFun';
 import Footer from './components/Footer';
 import Bar from './components/Bar';
+import Home from './page/Home';
+import Games from './page/Games';
+import Guess from './page/Guess';
+import Information from './page/Information';
 import './styles/App.less';
 
 class App extends React.Component {
@@ -23,6 +28,29 @@ class App extends React.Component {
     return (
       <div id="App">
         <Bar></Bar>
+        {/*  定义路由页面组件 */}
+        <Switch>
+          <Route Component={Home}
+            exact
+            path="/"
+          />
+          <Route Component={Home}
+            exact
+            path="/home"
+          />
+          <Route Component={Games}
+            exact
+            path="/games"
+          />
+          <Route Component={Guess}
+            exact
+            path="/Guess"
+          />
+          <Route Component={Information}
+            exact
+            path="/information"
+          />
+        </Switch>
         <Footer></Footer>
         hello, react!
       </div>
