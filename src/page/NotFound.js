38f6e69970258551be33a36ac1078f339f02a044
catonlan <catonlan@163.com>
2019-11-04 22:56:30 +0800

    修改404页面

--- src/page/NotFound.js
+++ src/page/NotFound.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NotFound from '../assets/image/404.jpg';
+
 
 class Guess extends React.Component {
   constructor(props) {
@@ -9,10 +9,6 @@ class Guess extends React.Component {
   render() {
     return (
       <div className="notFound">
-        {/* <img alt=""
-          src={NotFound}
-          style={{display: 'block', width: '100%', }}
-        /> */}
       </div>
     );
   }
